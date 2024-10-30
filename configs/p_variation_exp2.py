c2cef68de44c2099772a165f30ed85f9afecd889
Joern Wichmann <joern.wichmann@monash.edu>
2024-10-31 10:38:29 +1100

    configs and runner for p-variation experiments

---
+++ configs/p_variation_exp2.py
@@ -0,0 +1,28 @@
+"""Contains local parameter configuration."""
+### Experimentname
+NAME_EXPERIMENT: str = "p-variation_exp2"
+
+### P-VALUE
+P_VALUE = 2
+
+### Algorithm
+ALGORITHM_NAME: str =  "Crank Nicolson mixed FEM Stratonovich Transport Noise with anti-symmetrisation" #see src.algorithms.select.py for available choices
+
+################               FILE/DIRECTORY NAMES               ############################
+#Log
+NAME_LOGFILE_GENERATE: str = f"{NAME_EXPERIMENT}.log"
+
+#Vtk
+VTK_DIRECTORY: str = f"vtk"
+
+#Convergence
+TIME_DIRECTORYNAME: str = f"convergence_results/{NAME_EXPERIMENT}"
+
+#Stability
+STABILITY_DIRECTORYNAME: str = f"stability_results/{NAME_EXPERIMENT}"
+
+#Energy
+ENERGY_DIRECTORYNAME: str = f"energy_results/{NAME_EXPERIMENT}"
+
+#Statistics
+STATISTICS_DIRECTORYNAME: str = f"statistic_results/{NAME_EXPERIMENT}"
