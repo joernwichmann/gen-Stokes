8850710a88e34794b2d275b5a2eaab6245be55d4
Joern Wichmann <joern.wichmann@monash.edu>
2024-10-31 11:03:03 +1100

    changed to new names

--- data_processing/python-code-plot/configs.py
+++ data_processing/python-code-plot/configs.py
@@ -1,30 +1,30 @@
 '''Configs of all plots'''
 
 #### locations 
-ROOT_LOCATION = "../data/energy_results/"
+ROOT_LOCATION = "../../energy_results/"
 MEAN_LOCATION = "/kinetic_energy/"
 IND_LOCATION = "/individual/ind_kinetic_energy"
 DATA_SOURCE = "refinement_9.csv"
 
 #### stochatic
-NUMBER_SAMPLES = 1000
-NOISE_TYPES = ["intro-additive", "intro-multiplicative", "intro-transport"]
+NUMBER_SAMPLES = 10
+NOISE_TYPES = ["p-variation_exp1", "p-variation_exp2", "p-variation_exp3"]
 
 #### stationary
-STATIONARY_TIME = {"intro-additive": 0.13, "intro-multiplicative": 0.32, "intro-transport": 0.13}
+STATIONARY_TIME = {"p-variation_exp1": 0.13, "p-variation_exp2": 0.32, "p-variation_exp3": 0.13}
 STATIONARY_ENERGY = 0.04206492724892853
 
 #### plotting configs
 # colours
-COLOURS_MEAN = {"intro-additive": "#1b9e77", "intro-multiplicative": "#d95f02", "intro-transport": "#7570b3"}
-COLOURS_INDIVIDUAL  = {"intro-additive": "#66c2a5", "intro-multiplicative": "#fc8d62", "intro-transport": "#8da0cb"}
+COLOURS_MEAN = {"p-variation_exp1": "#1b9e77", "p-variation_exp2": "#d95f02", "p-variation_exp3": "#7570b3"}
+COLOURS_INDIVIDUAL  = {"p-variation_exp1": "#66c2a5", "p-variation_exp2": "#fc8d62", "p-variation_exp3": "#8da0cb"}
 BLACK = "#000000"
 
 # histogram plot
 HIST_DPI = 300
 HIST_FILEFORMAT = "pdf"
 
-YMAX =  {"intro-additive": 0.06, "intro-multiplicative": 0.016, "intro-transport": 0.014}
+YMAX =  {"p-variation_exp1": 0.06, "p-variation_exp2": 0.016, "p-variation_exp3": 0.014}
 LINEAR_PLOT = True
 LOG_PLOT = False
 
