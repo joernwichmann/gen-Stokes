cb3783a52039700e7d6ded9a84b040d87bd952f9
Joern Wichmann <joern.wichmann@monash.edu>
2024-11-04 12:20:26 +1100

    refactored point statistics objects as a process object

--- src/postprocess/point_statistics.py
+++ src/postprocess/point_statistics.py
@@ -1,93 +1,191 @@
-import numpy as np
-import os
-import shutil
 import csv
+from firedrake import Function
+import os
+from functools import cached_property
+from numpy import ndarray
 
+from src.utils import swap_dictionary_keys
+from src.discretisation.time import TimeDiscretisation
+from src.math.norms.stochastic import l1_stochastic, l2_stochastic, linf_stochastic
+from src.math.statistics import standard_deviation
+from src.plotter import plot_ref_to_time_to_function, plot_seed_to_time_to_number, plot_seed_to_time_to_number_and_increments
+from src.postprocess.processmanager import ProcessObject
 
+def _evaluate_funcAtpoint(time_to_function: dict[float,Function], point: list[float], func_dim: int) -> list[dict[float,float]]:
+    """Evaluate the dictionary "time -> function" at specified point. 
 
-def _update_mean(samples: int, old_mean_vector: np.ndarray, update_vector: np.ndarray) -> np.ndarray:
-    """Update the mean value."""
-    if not old_mean_vector.shape == update_vector.shape:
-        msg = "Shape of mean array und update array do not match."
-        msg += f"\nShape mean array:\t {old_mean_vector.shape}"
-        msg += f"\nShape update array:\t {update_vector.shape}"
-        raise ValueError(msg)
-    return samples/(samples + 1)*old_mean_vector + 1/(samples+1)*update_vector
+    Return 'component -> time -> function value'."""
+    return {component: {time: time_to_function[time].at(point)[component] for time in time_to_function.keys()} for component in range(func_dim)}
 
-def _update_square(samples: int, old_square_vector: np.ndarray, update_vector: np.ndarray) -> np.ndarray:
-    """Update the normalised second moment."""
-    if not old_square_vector.shape == update_vector.shape:
-        msg = "Shape of mean array und update array do not match."
-        msg += f"\nShape mean array:\t {old_square_vector.shape}"
-        msg += f"\nShape update array:\t {update_vector.shape}"
-        raise ValueError(msg)
-    return samples/(samples + 1)*old_square_vector + 1/(samples+1)*np.power(update_vector,2)
-
-
-
-class PointStatisticsObject:
-    """Class that contains utilities for the computation of mean, second moment, and deviation of 'ref -> time -> functionAtPoint' dictionary."""
-    def __init__(self, name: str, ref_to_time_grid: dict[int,list[float]], point: list[float]):
-        self.name = name
+class PointStatistics(ProcessObject):
+    """Class that contains tools for computing the energy."""
+    def __init__(self, 
+                 time_disc: TimeDiscretisation,
+                 point_name: str, 
+                 point: list[float],
+                 func_dim: int) -> None:
+        self.seed_to_ref_to_comp_to_time_to_value = dict()
+        self.seed_Id = 0
+        self.time_disc = time_disc
+        self.seed_to_ref_to_noise_increments = dict()
+        self.point_name = point_name
         self.point = point
-        self.point_dim = len(point)
-        self.ref_to_time_grid = ref_to_time_grid
-        self.ref_to_time_to_funcAtpoint_mean = {level: {time: np.ndarray((self.point_dim,)) for time in ref_to_time_grid[level]} for level in ref_to_time_grid.keys()}
-        self.ref_to_time_to_funcAtpoint_square = {level: {time: np.ndarray((self.point_dim,)) for time in ref_to_time_grid[level]} for level in ref_to_time_grid.keys()}
-        self.samples = 0
-        
+        self.func_dim = func_dim
 
-    def update(self,ref_to_time_to_function) -> None:
-        """Add a sample to mean and second moment."""
-        for level in self.ref_to_time_to_funcAtpoint_mean.keys():
-            for time in self.ref_to_time_to_funcAtpoint_mean[level].keys():
-                #update mean
-                self.ref_to_time_to_funcAtpoint_mean[level][time][:] = _update_mean(self.samples,
-                                                                                       self.ref_to_time_to_funcAtpoint_mean[level][time],
-                                                                                       ref_to_time_to_function[level][time].at(self.point))
-                #update square
-                self.ref_to_time_to_funcAtpoint_square[level][time][:] = _update_square(self.samples,
-                                                                                       self.ref_to_time_to_funcAtpoint_square[level][time],
-                                                                                       ref_to_time_to_function[level][time].at(self.point))
-        self.samples += 1
+    def update(self, ref_to_time_to_function: dict[int,dict[float,Function]], ref_to_noise_increments: list[int,ndarray],) -> None:
+        """Add an entry to the 'seed -> refinement level -> time -> energy ' dictionary."""
+        self.seed_to_ref_to_comp_to_time_to_value[self.seed_Id] = {level: _evaluate_funcAtpoint(ref_to_time_to_function[level],self.point,self.func_dim) 
+                                                       for level in ref_to_time_to_function.keys()}
+        self.seed_to_ref_to_noise_increments[self.seed_Id] = ref_to_noise_increments
+        self.seed_Id += 1
+
+    @cached_property
+    def ref_to_seed_to_comp_to_time_to_value(self):
+        if len(self.seed_to_ref_to_comp_to_time_to_value) == 0:
+            return dict()
+        return swap_dictionary_keys(self.seed_to_ref_to_comp_to_time_to_value)
+    
+    @cached_property
+    def ref_to_comp_to_seed_to_time_to_value(self):
+        if len(self.ref_to_seed_to_comp_to_time_to_value) == 0:
+            return dict()
+        return {level: swap_dictionary_keys(self.ref_to_seed_to_comp_to_time_to_value[level]) for level in self.ref_to_seed_to_comp_to_time_to_value}
+    
+    @cached_property
+    def ref_to_comp_to_time_to_seed_to_value(self):
+        if len(self.ref_to_comp_to_seed_to_time_to_value) == 0:
+            return dict()
+        return {level: {component: swap_dictionary_keys(self.ref_to_comp_to_seed_to_time_to_value[level][component]) 
+                        for component in self.ref_to_comp_to_seed_to_time_to_value[level]}  
+                        for level in self.ref_to_seed_to_comp_to_time_to_value}
+    
+    @cached_property
+    def ref_to_seed_to_noise_increments(self):
+        if len(self.seed_to_ref_to_noise_increments) == 0:
+            return dict()
+        return swap_dictionary_keys(self.seed_to_ref_to_noise_increments)
     
     @property
-    def ref_to_time_to_funcAtpoint_deviation(self) -> dict[int,dict[float,np.ndarray]]:
-        """Compute the standard deviation based on mean and second moment."""
-        ref_to_time_to_funcAtpoint_dev = {level: {time: np.ndarray((self.point_dim,)) for time in self.ref_to_time_grid[level]} for level in self.ref_to_time_grid.keys()}
-        for level in self.ref_to_time_to_funcAtpoint_mean.keys():
-            for time in self.ref_to_time_to_funcAtpoint_mean[level].keys():
-                #Analytically the deviation is always non-negative and there is no issue to take the square root. 
-                #But numerically it might happen that the deviation is negative, leading to difficulties. We avoid this by first truncating negative values to 0.
-                dev = self.ref_to_time_to_funcAtpoint_square[level][time] - np.power(self.ref_to_time_to_funcAtpoint_mean[level][time],2)
-                zeros = np.zeros(dev.shape)
-                dev_zeros = np.maximum(dev,zeros)
-                ref_to_time_to_funcAtpoint_dev[level][time][:] = np.sqrt(dev_zeros)
-        return ref_to_time_to_funcAtpoint_dev
-
-    def _save(self, name_directory: str) -> None:
-        """Save mean and standard deviation in csv format."""
-        header = ["time"] + [f"Mean_{comp}" for comp in range(self.point_dim)] + [f"SD_{comp}" for comp in range(self.point_dim)]
+    def ref_to_comp_to_time_to_value_l1(self) -> dict[int,dict[int,dict[float,list[float]]]]:
+        ref_to_time_to_funcAtpoint = {level: {component: {time: l1_stochastic(self.ref_to_comp_to_time_to_seed_to_value[level][component][time].values()) 
+                                                          for time in self.ref_to_comp_to_time_to_seed_to_value[level][component]} 
+                                              for component in self.ref_to_comp_to_time_to_seed_to_value[level]} 
+                                for level in self.ref_to_comp_to_time_to_seed_to_value}
+        return ref_to_time_to_funcAtpoint
+    
+    @property
+    def ref_to_comp_to_time_to_value_l2(self) -> dict[int,dict[int,dict[float,list[float]]]]:
+        ref_to_time_to_funcAtpoint = {level: {component: {time: l2_stochastic(self.ref_to_comp_to_time_to_seed_to_value[level][component][time].values()) 
+                                                          for time in self.ref_to_comp_to_time_to_seed_to_value[level][component]} 
+                                              for component in self.ref_to_comp_to_time_to_seed_to_value[level]} 
+                                for level in self.ref_to_comp_to_time_to_seed_to_value}
+        return ref_to_time_to_funcAtpoint
+    
+    @property
+    def ref_to_comp_to_time_to_value_linf(self) -> dict[int,dict[int,dict[float,list[float]]]]:
+        ref_to_time_to_funcAtpoint = {level: {component: {time: linf_stochastic(self.ref_to_comp_to_time_to_seed_to_value[level][component][time].values()) 
+                                                          for time in self.ref_to_comp_to_time_to_seed_to_value[level][component]} 
+                                              for component in self.ref_to_comp_to_time_to_seed_to_value[level]} 
+                                for level in self.ref_to_comp_to_time_to_seed_to_value}
+        return ref_to_time_to_funcAtpoint
+    
+    @property
+    def ref_to_comp_to_time_to_value_SD(self) -> dict[int,dict[int,dict[float,list[float]]]]:
+        ref_to_time_to_funcAtpoint = {level: {component: {time: standard_deviation(self.ref_to_comp_to_time_to_seed_to_value[level][component][time].values()) 
+                                                          for time in self.ref_to_comp_to_time_to_seed_to_value[level][component]} 
+                                              for component in self.ref_to_comp_to_time_to_seed_to_value[level]} 
+                                for level in self.ref_to_comp_to_time_to_seed_to_value}
+        return ref_to_time_to_funcAtpoint
+    
+    def save(self, name_directory: str) -> None:
+        """Save 'time -> funcAtpoint' in .csv files."""
+        header = ["time"]
+        header += [f"L1_{component}" for component in range(self.func_dim)]
+        header += [f"L2_{component}" for component in range(self.func_dim)]
+        header += [f"Linf_{component}" for component in range(self.func_dim)]
+        header += [f"SD_{component}" for component in range(self.func_dim)]
         ref_to_data = {}
-        for level in self.ref_to_time_to_funcAtpoint_mean.keys():
-            ref_to_data[level] = [[time] + [self.ref_to_time_to_funcAtpoint_mean[level][time][comp] for comp in range(self.point_dim)] + [self.ref_to_time_to_funcAtpoint_deviation[level][time][comp] for comp in range(self.point_dim)]
-                                   for time in self.ref_to_time_to_funcAtpoint_mean[level].keys()]
+        for level in self.time_disc.refinement_levels:
+            data = []
+            for time in self.time_disc.ref_to_time_grid[level]:
+                l1 = []
+                l2 = []
+                linf = []
+                SD = []
+                for component in range(self.func_dim):
+                    l1.append(self.ref_to_comp_to_time_to_value_l1[level][component][time])
+                    l2.append(self.ref_to_comp_to_time_to_value_l2[level][component][time])
+                    linf.append(self.ref_to_comp_to_time_to_value_linf[level][component][time])
+                    SD.append(self.ref_to_comp_to_time_to_value_SD[level][component][time])
+                data.append([time] + l1 + l2 + linf + SD)
+            ref_to_data[level] = data
             
         if not os.path.isdir(name_directory):
             os.makedirs(name_directory)
 
-        for level in self.ref_to_time_to_funcAtpoint_mean.keys():
-            outfile = name_directory + "/refinement_" + str(level) + ".csv"
+        new_dict_name = name_directory + "/" + self.point_name + f"/mean"
+        if not os.path.isdir(new_dict_name):
+            os.makedirs(new_dict_name)
+
+        for level in self.time_disc.refinement_levels:
+            outfile = new_dict_name + "/refinement_" + str(level) + ".csv"
             with open(outfile,"w",newline="") as file:
                 writer = csv.writer(file)
                 writer.writerow(header)
                 writer.writerows(ref_to_data[level])
 
+    def save_individual(self, name_directory: str, requested_samples: int) -> None:
+        """Save 'time -> funcAtpoint' in .csv files."""
+        #construct header
+        header = ["time"]
+        header += [f"f(point)_{component}" for component in range(self.func_dim)]
 
-    def save(self,name_directory: str) -> None:
-        """Save mean and standard deviation in vtk format."""
-        save_directory = name_directory + "/" + self.name
-        #remove old data
-        if os.path.isdir(save_directory):
-                shutil.rmtree("./" + save_directory)
-        self._save(save_directory)
+        #construct data
+        seed_to_ref_to_data = dict()
+        for sample in range(min(requested_samples,self.seed_Id)):
+            ref_to_data = {}
+            for level in self.time_disc.refinement_levels:
+                data = []
+                for time in self.time_disc.ref_to_time_grid[level]:
+                    value = []
+                    for component in range(self.func_dim):
+                        value.append(self.ref_to_comp_to_time_to_seed_to_value[level][component][time][sample])
+                    data.append([time] + value)
+                ref_to_data[level] = data
+            seed_to_ref_to_data[sample] = ref_to_data
+        
+        #check if directory is avaiable
+        if not os.path.isdir(name_directory):
+            os.makedirs(name_directory)
+
+        #write data
+        for sample in range(min(requested_samples,self.seed_Id)):
+            new_dict_name = name_directory + "/" + self.point_name + f"/seed_{sample}"
+            if not os.path.isdir(new_dict_name):
+                os.makedirs(new_dict_name)
+
+                for level in self.time_disc.refinement_levels:
+                    outfile = new_dict_name + "/refinement_" + str(level) + ".csv"
+                    with open(outfile,"w",newline="") as file:
+                        writer = csv.writer(file)
+                        writer.writerow(header)
+                        writer.writerows(seed_to_ref_to_data[sample][level])
+
+    def plot(self, name_directory: str) -> None:
+        return
+    #    """Save 'time -> energy'."""
+    #    if not os.path.isdir(name_directory):
+    #        os.makedirs(name_directory)
+
+    #    new_dict_name = name_directory + "/" + self.energy_name 
+    #    if not os.path.isdir(new_dict_name):
+    #        os.makedirs(new_dict_name)
+    #    plot_ref_to_time_to_function({self.energy_name + "_L1": self.ref_to_time_to_energy_l1, 
+    #                                  self.energy_name + "_Deviation": self.ref_to_time_to_energy_deviation},new_dict_name + "/L1.png",["log","log"])
+    #    plot_ref_to_time_to_function({self.energy_name + "_L2": self.ref_to_time_to_energy_l2, 
+    #                                  self.energy_name + "_Deviation": self.ref_to_time_to_energy_deviation},new_dict_name + "/L2.png",["log","log"])
+    #    plot_ref_to_time_to_function({self.energy_name + "_Linf": self.ref_to_time_to_energy_linf, 
+    #                                  self.energy_name + "_Deviation": self.ref_to_time_to_energy_deviation},new_dict_name + "/Linf.png",["log","log"])
+
+    def plot_individual(self, name_directory: str) -> None:
+        return
