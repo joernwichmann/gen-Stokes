cf516f24164eeccc3f457252c2213a3ee7b1992a
Joern Wichmann <joern.wichmann@monash.edu>
2024-11-07 09:12:26 +1100

    adapted to new data structure

--- data_processing/python-code-plot/generate-traj-point-statistics.py
+++ data_processing/python-code-plot/generate-traj-point-statistics.py
@@ -4,7 +4,7 @@ from tools_point_statistics import read_datafile, organize_output, organize_outp
 from tqdm import tqdm
 
 ### select the experiments whose data will be visualised 
-from configs import p_variation_point_statistics as cf
+from configs import lid_driven_point_statistics as cf
 
 if __name__=="__main__":
     print(f"Start plot of trajectories in dataformat '.{cf.TRAJ_FILEFORMAT}' with dpi '{cf.TRAJ_DPI}'")
@@ -31,20 +31,10 @@ if __name__=="__main__":
     for expID in cf.EXPERIMENTS.keys():
         file_location = cf.ROOT_LOCATION + cf.EXPERIMENTS[expID] + cf.MEAN_LOCATION + cf.DATA_SOURCE
         complete_data = read_datafile(file_location)
-        time, l1_0, l1_1, l2_0, l2_1, linf_0, linf_1, sd_0, sd_1 = organize_output(complete_data)
-        l2_0_plus = []
-        l2_0_minus = []
-        l2_1_plus = []
-        l2_1_minus = []
-        for _l2, _sd in zip(l2_0, sd_0):
-            l2_0_plus.append(_l2 + _sd)
-            l2_0_minus.append(_l2-_sd)
-        for _l2, _sd in zip(l2_1, sd_1):
-            l2_1_plus.append(_l2 + _sd)
-            l2_1_minus.append(_l2-_sd)
+        time, mean_0, mean_1, sd_0, sd_1 = organize_output(complete_data)
 
         #mean energy trajectory
-        plt.plot(l1_0,l1_1,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_MEAN,alpha=cf.LINEOPACITY_MEAN)
+        plt.plot(mean_0,mean_1,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_MEAN,alpha=cf.LINEOPACITY_MEAN)
         #standard deviation
         #plt.plot(l2_0_plus,l2_1_plus,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_SD,alpha=cf.LINEOPACITY_SD,linestyle=cf.LINESTYLE_SD)
         #plt.plot(l2_0_plus,l2_1_minus,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_SD,alpha=cf.LINEOPACITY_SD,linestyle=cf.LINESTYLE_SD)
@@ -54,8 +44,8 @@ if __name__=="__main__":
         #plot det
         file_location = cf.ROOT_LOCATION + cf.EXPERIMENTS[expID] + cf.DET_LOCATION + cf.DATA_SOURCE
         complete_data = read_datafile(file_location)
-        time, l1_0, l1_1, l2_0, l2_1, linf_0, linf_1, sd_0, sd_1 = organize_output(complete_data)
-        plt.plot(l1_0,l1_1,color = cf.BLACK,linewidth=1,alpha=cf.LINEOPACITY_MEAN)
+        time, mean_0, mean_1, sd_0, sd_1 = organize_output(complete_data)
+        plt.plot(mean_0,mean_1,color = cf.BLACK,linewidth=1,alpha=cf.LINEOPACITY_MEAN)
         plt.plot(cf.STATIONARY_VAL_X[expID],cf.STATIONARY_VAL_Y[expID], marker = "o",color=cf.COLOURS_MEAN[expID])
         
         #define legend
@@ -65,7 +55,7 @@ if __name__=="__main__":
     # styling the plot
     plt.ylabel("y")
     plt.xlabel("x")
-    plt.title('velocity @ [0.5,0.75]')
+    plt.title('velocity at (0.5,0.75)')
     #plt.xlim(0.0,1.0)
     plt.legend(legendMarkers,legendEntries)
     plt.tight_layout()
@@ -92,20 +82,10 @@ if __name__=="__main__":
         print(f"\tPlotting mean and standard devations")
         file_location = cf.ROOT_LOCATION + cf.EXPERIMENTS[expID] + cf.MEAN_LOCATION + cf.DATA_SOURCE
         complete_data = read_datafile(file_location)
-        time, l1_0, l1_1, l2_0, l2_1, linf_0, linf_1, sd_0, sd_1 = organize_output(complete_data)
-        l2_0_plus = []
-        l2_0_minus = []
-        l2_1_plus = []
-        l2_1_minus = []
-        for _l2, _sd in zip(l2_0, sd_0):
-            l2_0_plus.append(_l2 + _sd)
-            l2_0_minus.append(_l2-_sd)
-        for _l2, _sd in zip(l2_1, sd_1):
-            l2_1_plus.append(_l2 + _sd)
-            l2_1_minus.append(_l2-_sd)
+        time, mean_0, mean_1, sd_0, sd_1  = organize_output(complete_data)
 
         #mean energy trajectory
-        plt.plot(l1_0,l1_1,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_MEAN,alpha=cf.LINEOPACITY_MEAN)
+        plt.plot(mean_0,mean_1,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_MEAN,alpha=cf.LINEOPACITY_MEAN)
         #standard deviation
         #plt.plot(l2_0_plus,l2_1_plus,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_SD,alpha=cf.LINEOPACITY_SD,linestyle=cf.LINESTYLE_SD)
         #plt.plot(l2_0_plus,l2_1_minus,color = cf.COLOURS_MEAN[expID],linewidth=cf.LINEWIDTH_SD,alpha=cf.LINEOPACITY_SD,linestyle=cf.LINESTYLE_SD)
@@ -115,15 +95,15 @@ if __name__=="__main__":
         print(f"\tPlotting deterministic energy")
         file_location = cf.ROOT_LOCATION + cf.EXPERIMENTS[expID] + cf.DET_LOCATION + cf.DATA_SOURCE
         complete_data = read_datafile(file_location)
-        time, l1_0, l1_1, l2_0, l2_1, linf_0, linf_1, sd_0, sd_1 = organize_output(complete_data)
-        plt.plot(l1_0,l1_1,color = cf.BLACK,linewidth=1,alpha=cf.LINEOPACITY_MEAN)
+        time, mean_0, mean_1, sd_0, sd_1 = organize_output(complete_data)
+        plt.plot(mean_0,mean_1,color = cf.BLACK,linewidth=1,alpha=cf.LINEOPACITY_MEAN)
         plt.plot(cf.STATIONARY_VAL_X[expID],cf.STATIONARY_VAL_Y[expID], marker = "o",color=cf.COLOURS_MEAN[expID])
         
 
         ### styling the plot
         plt.ylabel("y", fontsize=cf.LABEL_FONTSIZE)
         plt.xlabel("x", fontsize=cf.LABEL_FONTSIZE)
-        plt.title('velocity @ [0.5,0.75]', fontsize=cf.LABEL_FONTSIZE)
+        plt.title('velocity at (0.5,0.75)', fontsize=cf.LABEL_FONTSIZE)
         plt.yticks(fontsize=cf.TICK_FONTSIZE)
         plt.xticks(fontsize=cf.TICK_FONTSIZE)
         #plt.xlim(0.0,1.0)
